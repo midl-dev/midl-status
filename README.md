ee6653e3712df1ce38520cb85a05a6fba923220c
Chao Zhang <charles.zhangzs@gmail.com>
2023-08-23 17:58:13 +0200

    create gh-pages branch

--- README.md
+++
@@ -1,7 +0,0 @@
-# midl-status
-
-## Workflow
-
-This is an flask app that we deploy to collect and mine useful data display our service status
-
-![workflow](./app.png)
--- app/blueprints/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-from app.blueprints.home import home_bp
-
-
-def register_blueprints(app: Flask) -> None:
-    app.register_blueprint(home_bp)
